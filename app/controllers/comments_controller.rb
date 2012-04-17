7584c88ed0503272af96312636740fe2b91cdad0
nhahn <Nathan.P.Hahn@boeing.com>
2012-04-16 20:44:27 -0400

    Added the nice forms to everything

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,83 +1,64 @@
 class CommentsController < ApplicationController
-  # GET /comments
-  # GET /comments.json
+  
   def index
     @comments = Comment.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @comments }
-    end
   end
-
-  # GET /comments/1
-  # GET /comments/1.json
+  
   def show
     @comment = Comment.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @comment }
-    end
   end
-
-  # GET /comments/new
-  # GET /comments/new.json
+  
   def new
     @comment = Comment.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @comment }
+  end
+  
+  def create
+    @commentable = find_commentable
+    @comment = @commentable.comments.build(params[:comment])
+    if @comment.save
+      flash[:notice] = "Successfully created comment."
+      # We'd like to go back to the page where the comment was made.  To 
+      # do this we need to know what type of item we were commenting on 
+      # and what particular item was being commented.
+      commenting_on = @comment.commentable_type.pluralize
+      id = @comment.commentable_id
+      # until the asset pipeline came along, the following worked well to get there:
+      # redirect_to :controller => commenting_on, :action => 'show', :id => id
+      # instead, we will simply use the following eval hack to go back to the page:
+      eval "redirect_to #{@comment.commentable_type.downcase}_path(:id=>#{id})"
+    else
+      render :action => 'new'
     end
   end
-
-  # GET /comments/1/edit
+  
   def edit
     @comment = Comment.find(params[:id])
   end
-
-  # POST /comments
-  # POST /comments.json
-  def create
-    @comment = Comment.new(params[:comment])
-
-    respond_to do |format|
-      if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
-        format.json { render json: @comment, status: :created, location: @comment }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /comments/1
-  # PUT /comments/1.json
+  
   def update
     @comment = Comment.find(params[:id])
-
-    respond_to do |format|
-      if @comment.update_attributes(params[:comment])
-        format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
+    if @comment.update_attributes(params[:comment])
+      flash[:notice] = "Successfully updated comment."
+      redirect_to @comment
+    else
+      render :action => 'edit'
     end
   end
-
-  # DELETE /comments/1
-  # DELETE /comments/1.json
+  
   def destroy
     @comment = Comment.find(params[:id])
     @comment.destroy
-
-    respond_to do |format|
-      format.html { redirect_to comments_url }
-      format.json { head :no_content }
-    end
+    flash[:notice] = "Successfully destroyed comment."
+    redirect_to comments_url
   end
+    
+  private
+  def find_commentable
+    params.each do |name, value|
+      if name =~ /(.+)_id$/
+        return $1.classify.constantize.find(value)
+      end
+    end
+    nil
+  end  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,16 @@ KcwWorld::Application.routes.draw do
 
   resources :comments
 
-  resources :articles
   resources :categories
-  resources :photos
   resources :proverbs
+  
+  resources :articles do
+    resources :comments
+  end
+
+  resources :photos do
+    resources :comments
+  end
   
   match 'home' => 'home#index', :as => :home
   root :to => 'home#index'
